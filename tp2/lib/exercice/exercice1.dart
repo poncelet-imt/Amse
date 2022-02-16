f195b06907dcdbd0e2a49943e971c2c0bde9b9de
poncelet-imt <thibault.poncelet@etu.imt-nord-europe.fr>
2022-02-16 06:19:32 +0100

    update exercice 1

--- tp2/lib/exercice/exercice1.dart
+++ tp2/lib/exercice/exercice1.dart
@@ -3,15 +3,13 @@ import 'package:flutter/material.dart';
 
 
 class Exercice1Page extends StatelessWidget {
-  // In the constructor, require a Todo.
   const Exercice1Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Use the Todo to create the UI.
     return Scaffold(
       appBar: AppBar(
-        title: Text("Exercice 1"),
+        title: const Text("Exercice 1"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
